3824b923d473e96fc1a684b931c352fc6f4051c8
Emin Ates <223301009@ogr.selcuk.edu.tr>
2024-10-26 17:11:19 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int ScoreOfString(string s)
+{
+    int score = 0;
+    int letterScore = 0;
+    foreach (char c in s)
+    {
+
+        if (letterScore == 0)
+            letterScore = System.Convert.ToInt32(c);
+        else if (letterScore == System.Convert.ToInt32(c))
+        {
+
+        }
+        else
+        {
+            score = score + Math.Abs(letterScore - System.Convert.ToInt32(c));
+            letterScore = System.Convert.ToInt32(c);
+        }
+    }
+
+    return score;
+}
